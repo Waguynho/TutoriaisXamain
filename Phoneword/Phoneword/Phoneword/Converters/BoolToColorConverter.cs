bc0134420b919efa3fce20978f497c638743ccbe
Wagner dos Santos <sygnw@yahoo.com.br>
2018-05-28 02:20:28 -0300

    DataTemplate Advanced with IValueConverter

---
+++ Phoneword/Phoneword/Phoneword/Converters/BoolToColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Phoneword.Converters
+{
+    public class BoolToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() == typeof(bool))
+            {
+                return (bool)value == true ? Color.Blue : Color.Red;
+            }
+
+            return Color.Blue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.GetType() == typeof(bool))
+            {
+              return  (bool)value == true ? Color.Red : Color.Blue;
+            }
+
+            return Color.Red;
+        }
+    }
+}
--- Phoneword/Phoneword/Phoneword/HelloMonsterView.cs
+++ Phoneword/Phoneword/Phoneword/HelloMonsterView.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using Phoneword.Converters;
+using Xamarin.Forms;
 
 namespace Phoneword
 {
@@ -11,21 +12,45 @@ namespace Phoneword
                 RowHeight = 40
             };
             listView.ItemsSource = new TodoItem[] {
-                new TodoItem { Name = "Buy pears" },
-                new TodoItem { Name = "Buy oranges", Done=true} ,
-                new TodoItem { Name = "Buy mangos" },
-                new TodoItem { Name = "Buy apples", Done=true },
-                new TodoItem { Name = "Buy bananas", Done=true }
+                new TodoItem { Name = "Buy pears", Done = false },
+                new TodoItem { Name = "Buy oranges", Done= true} ,
+                new TodoItem { Name = "Buy mangos"  , Done = false },
+                new TodoItem { Name = "Buy apples", Done= true },
+                new TodoItem { Name = "Buy bananas", Done= true }
             };
 
-            listView.ItemTemplate = new DataTemplate(typeof(TextCell));
-            listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+            var dataTemplate = new DataTemplate();
+
+            listView.ItemTemplate = GetDataTemplate();
 
             Content = new StackLayout
             {
                 VerticalOptions = LayoutOptions.FillAndExpand,
                 Children = { listView }
             };
+        }
+
+        private DataTemplate GetDataTemplate()
+        {
+
+            return new DataTemplate(() =>
+             {
+                 var grid = new Grid();
+
+                 var task = new Label { FontAttributes = FontAttributes.Bold };
+                 var done = new Label();
+
+                 task.SetBinding(Label.TextProperty, "Name");
+                 done.SetBinding(Label.TextProperty, "Done");
+                 var convertColor = new BoolToColorConverter();
+                 convertColor.Convert(this, typeof(Color), null, null);
+                 done.SetBinding(Label.TextColorProperty, new Binding("Done", BindingMode.TwoWay, convertColor));
+
+                 grid.Children.Add(task, 0, 0);
+                 grid.Children.Add(done, 1, 0);
+
+                 return new ViewCell { View = grid };
+             });
         }
     }
 }
