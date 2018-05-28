c56b8206f0ef07ee44a9f3af6e3dbbe7d6a3dfef
Wagner dos Santos <sygnw@yahoo.com.br>
2018-05-27 23:30:25 -0300

    List with Complex Object

--- Phoneword/Phoneword/Phoneword/HelloMonsterView.cs
+++ Phoneword/Phoneword/Phoneword/HelloMonsterView.cs
@@ -10,10 +10,17 @@ namespace Phoneword
             {
                 RowHeight = 40
             };
-            listView.ItemsSource = new string[]
-            {
-                "Buy pears", "Buy oranges", "Buy mangos", "Buy apples", "Buy bananas"
+            listView.ItemsSource = new TodoItem[] {
+                new TodoItem { Name = "Buy pears" },
+                new TodoItem { Name = "Buy oranges", Done=true} ,
+                new TodoItem { Name = "Buy mangos" },
+                new TodoItem { Name = "Buy apples", Done=true },
+                new TodoItem { Name = "Buy bananas", Done=true }
             };
+
+            listView.ItemTemplate = new DataTemplate(typeof(TextCell));
+            listView.ItemTemplate.SetBinding(TextCell.TextProperty, "Name");
+
             Content = new StackLayout
             {
                 VerticalOptions = LayoutOptions.FillAndExpand,
---
+++ Phoneword/Phoneword/Phoneword/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Phoneword
+{
+    public class TodoItem
+    {
+        public string Name { get; set; }
+        public bool Done { get; set; }
+    }
+}
