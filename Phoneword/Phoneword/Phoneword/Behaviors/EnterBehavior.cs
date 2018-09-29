31121eb0ee0571ec346077d2b8c861683aabd217
Wagner dos Santos <sygnw@yahoo.com.br>
2018-09-29 00:36:45 -0300

    Behaviors -  Part 1

---
+++ Phoneword/Phoneword/Phoneword/Behaviors/EnterBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Phoneword.Behaviors
+{
+    public class EnterBehavior : Behavior<Entry>
+    {
+        protected override void OnAttachedTo(Entry entry)
+        {
+            entry.Completed += RerurnPressed;
+            entry.Unfocused += OnUnfocused;
+            base.OnAttachedTo(entry);
+        }
+
+        private void OnUnfocused(object sender, FocusEventArgs e)
+        {
+            ((Entry)sender).TextColor = Color.Default;
+        }
+
+        protected override void OnDetachingFrom(Entry entry)
+        {
+            entry.Unfocused -= OnUnfocused;
+            entry.Completed -= RerurnPressed;
+            base.OnDetachingFrom(entry);
+        }
+
+        private void RerurnPressed(object sender, EventArgs e)
+        {
+            ((Entry)sender).TextColor = Color.Red;
+        }
+    }
+}
