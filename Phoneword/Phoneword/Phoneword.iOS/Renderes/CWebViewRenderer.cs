0f335420c974fa72f5a61b8cb7663474160f4cd9
Wagner dos Santos <wagner.santos@sigga.com>
2020-05-07 17:00:27 -0300

    WKWebView - IOS

---
+++ Phoneword/Phoneword/Phoneword.iOS/Renderes/CWebViewRenderer.cs
@@ -0,0 +1,16 @@
+﻿using Phoneword.iOS.Renderes;
+using WebKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(WKWebView), typeof(CWebViewRenderer))]
+namespace Phoneword.iOS.Renderes
+{
+    public class CWebViewRenderer : WkWebViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+        }
+    }
+}
