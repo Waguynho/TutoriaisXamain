fc95816c9ed86d41d2d2dfc3c9126ea3bbb54cfb
Wagner dos Santos <wagner.santos@sigga.com>
2021-02-03 14:13:04 -0300

    Allow Copy label in Android

---
+++ Phoneword/Phoneword/Phoneword.Android/Renders/WLabelRender.cs
@@ -0,0 +1,35 @@
+﻿using Android.Content;
+using Android.Views;
+using Phoneword.Controls;
+using Phoneword.Droid.Renders;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+[assembly: ExportRenderer(typeof(Label), typeof(WLabelRender))]
+namespace Phoneword.Droid.Renders
+{
+    class WLabelRender : LabelRenderer
+    {
+        public WLabelRender(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null && e.NewElement != null)
+            {
+                Control.SetTextIsSelectable(true);
+            }
+        }
+
+        private void SetInvertedFlow(WEntry wEntry)
+        {
+            if (wEntry.InvertedFlow)
+            {
+                Control.LayoutDirection = LayoutDirection.Rtl;
+                Control.TextDirection = TextDirection.Rtl;
+            }        
+        }
+    }
+}
