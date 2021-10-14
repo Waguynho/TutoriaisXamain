c0727129b4c257ea1dd7f70211f35571e7883077
wagner.santos <wagner.santos@sigga.com>
2021-10-14 18:27:53 -0300

    Reader bar code in IOS - (I will rewrite in android)

---
+++ Phoneword/Phoneword/Phoneword.iOS/DependencyService/BarCodeReader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Phoneword.iOS.DependencyService;
+using Phoneword.Utils;
+using Xamarin.Forms;
+using ZXing.Mobile;
+
+
+[assembly: Dependency(typeof(BarCodeReader))]
+namespace Phoneword.iOS.DependencyService
+{
+    public class BarCodeReader: IBarCodeReader
+    {
+        private MobileBarcodeScanner scanner;
+        public Action<string> ResultCallBack { get; set; }
+
+        public BarCodeReader()
+        {
+        }
+
+        public async Task Scan()
+        {
+            scanner = new ZXing.Mobile.MobileBarcodeScanner();
+
+            var options = GetFormats();
+
+            var result = await scanner.Scan(options);
+
+            if (result != null && ResultCallBack != null)
+            {
+                ResultCallBack.Invoke(result.Text);
+            }
+        }
+
+        private MobileBarcodeScanningOptions GetFormats()
+        {
+            var options = new MobileBarcodeScanningOptions();
+            options.PossibleFormats = new System.Collections.Generic.List<ZXing.BarcodeFormat> {
+                ZXing.BarcodeFormat.CODABAR,
+                ZXing.BarcodeFormat.CODE_128,
+                ZXing.BarcodeFormat.DATA_MATRIX,
+                ZXing.BarcodeFormat.EAN_13,
+                ZXing.BarcodeFormat.QR_CODE,
+                ZXing.BarcodeFormat.EAN_8,
+                ZXing.BarcodeFormat.ITF
+            };
+            return options;
+        }
+    }
+}
